0dba511685bebcd75008d20f0cf102a9e8826a6a
Danny Zorin <zorin.dania@yandex.ru>
2017-10-15 18:54:41 +0300

    [D84aPyrz] Added Speech and Speaker models

--- Sources/App/Models/Speaker/Speaker.swift
+++ Sources/App/Models/Speaker/Speaker.swift
@@ -2,21 +2,69 @@ import Vapor
 import FluentProvider
 
 final class Speaker: Model {
+    
     let storage = Storage()
     
-    // MARK: Properties and database keys
+    var userId: Node
+    var eventId: Node
+    
+    struct Keys {
+        static let id = "id"
+        static let userId = "user_id"
+        static let eventId = "event_id"
+    }
+    
+    init(userId: Node, eventId: Node) {
+        self.userId = userId
+        self.eventId = eventId
+    }
     
     // MARK: Fluent Serialization
     
-    /// Initializes the Speaker from the
-    /// database row
     init(row: Row) throws {
-        //
+        userId = try row.get(Speaker.Keys.userId)
+        eventId = try row.get(Speaker.Keys.eventId)
     }
     
-    // Serializes the Speaker to the database
     func makeRow() throws -> Row {
         var row = Row()
+        try row.set(Speaker.Keys.userId, userId)
+        try row.set(Speaker.Keys.eventId, eventId)
         return row
     }
 }
+
+// MARK: Fluent Preparation
+
+extension Speaker: Preparation {
+    
+    static func prepare(_ database: Database) throws {
+        try database.create(self) { builder in
+            builder.id()
+            // Uncomment when User model is created
+            //builder.foreignKey(Speaker.Keys.userId, references: User.Keys.id, on: User.self, named: "user")
+            builder.foreignKey(Speaker.Keys.eventId, references: Event.Keys.id, on: Event.self, named: "event")
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
+
+// MARK: JSON
+
+extension Speaker: JSONRepresentable {
+    
+    func makeJSON() throws -> JSON {
+        var json = JSON()
+        try json.set(Speaker.Keys.id, id)
+        try json.set(Speaker.Keys.userId, userId)
+        try json.set(Speaker.Keys.eventId, eventId)
+        return json
+    }
+}
+
+// MARK: HTTP
+
+extension Speaker: ResponseRepresentable { }
