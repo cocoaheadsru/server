982bc657cc5d6a0741b1ea449da3ea3577e78620
Danny Zorin <zorin.dania@yandex.ru>
2017-10-15 23:45:18 +0300

    [D84aPyrz] Created empty form-based models

---
+++ Sources/App/Models/EventReg/EventReg.swift
@@ -0,0 +1,22 @@
+import Vapor
+import FluentProvider
+
+final class EventReg: Model {
+    
+    let storage = Storage()
+    
+    struct Keys {
+        static let id = "id"
+    }
+    
+    // MARK: Fluent Serialization
+    
+    init(row: Row) throws {
+        //
+    }
+    
+    func makeRow() throws -> Row {
+        var row = Row()
+        return row
+    }
+}
---
+++ Sources/App/Models/EventRegAnswer/EventRegAnswer.swift
@@ -0,0 +1,22 @@
+import Vapor
+import FluentProvider
+
+final class EventRegAnswer: Model {
+    
+    let storage = Storage()
+    
+    struct Keys {
+        static let id = "id"
+    }
+    
+    // MARK: Fluent Serialization
+    
+    init(row: Row) throws {
+        //
+    }
+    
+    func makeRow() throws -> Row {
+        var row = Row()
+        return row
+    }
+}
