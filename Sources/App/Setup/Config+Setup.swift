9368d0837f0ac2cac1e9b8448ed2e7a48780f92b
Peter Tretyakov <petertretyakov@gmail.com>
2017-11-02 13:52:13 +0300

    [Y22yo0q9] Added User and Client to DB preparations, Removed Post entity from DB preparations

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -20,6 +20,7 @@ extension Config {
   /// Add all models that should have their
   /// schemas prepared before the app boots
   private func setupPreparations() throws {
-    preparations.append(Post.self)
+    preparations.append(User.self)
+    preparations.append(Client.self)
   }
 }
