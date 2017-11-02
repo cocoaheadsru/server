c6ac54313c8a18813d6b2e4b9afd445ede854a66
Peter Tretyakov <petertretyakov@gmail.com>
2017-11-02 13:50:49 +0300

    [Y22yo0q9] Re-indent default Vapor files (Routes, Setup)

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
-
-    }
+  func setupRoutes() throws {
+    
+  }
 }
--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -2,24 +2,24 @@ import FluentProvider
 import MySQLProvider
 
 extension Config {
-    public func setup() throws {
-        // allow fuzzy conversions for these types
-        // (add your own types here)
-        Node.fuzzy = [Row.self, JSON.self, Node.self]
-
-        try setupProviders()
-        try setupPreparations()
-    }
+  public func setup() throws {
+    // allow fuzzy conversions for these types
+    // (add your own types here)
+    Node.fuzzy = [Row.self, JSON.self, Node.self]
     
-    /// Configure providers
-    private func setupProviders() throws {
-        try addProvider(FluentProvider.Provider.self)
-        try addProvider(MySQLProvider.Provider.self)
-    }
-    
-    /// Add all models that should have their
-    /// schemas prepared before the app boots
-    private func setupPreparations() throws {
-        preparations.append(Post.self)
-    }
+    try setupProviders()
+    try setupPreparations()
+  }
+  
+  /// Configure providers
+  private func setupProviders() throws {
+    try addProvider(FluentProvider.Provider.self)
+    try addProvider(MySQLProvider.Provider.self)
+  }
+  
+  /// Add all models that should have their
+  /// schemas prepared before the app boots
+  private func setupPreparations() throws {
+    preparations.append(Post.self)
+  }
 }
