cd6036f3ad1a8a5e1e8b722959265b6e6d6ad4eb
Peter Tretyakov <petertretyakov@gmail.com>
2017-11-02 14:10:56 +0300

    [Y22yo0q9] Added BaseController implementation - associated type - enum Method - default methods (index, show, create, destroy) in extension

--- Sources/App/Controllers/BaseController.swift
+++ Sources/App/Controllers/BaseController.swift
@@ -1,3 +1,58 @@
+import Vapor
+import FluentProvider
+
 protocol BaseController {
+  associatedtype Resource: Model, JSONConvertible, ResponseRepresentable
+}
+
+enum Method {
+  case index
+  case show
+  case create
+  case destroy
+}
+
+extension BaseController {
   
+  func addRoutes(to droplet: Droplet, namespace: String? = nil, only methods: [Method] = [.index, .show, .create, .destroy]) {
+    let modelRoute = namespace ?? String(describing: Resource.self).lowercased()
+    let resourceGroup = droplet.grouped(modelRoute)
+    for method in methods {
+      switch method {
+      case .index:
+        resourceGroup.get(handler: index)
+      case .show:
+        resourceGroup.get(Resource.parameter, handler: show)
+      case .create:
+        resourceGroup.post(handler: create)
+      case .destroy:
+        resourceGroup.delete(Resource.parameter, handler: destroy)
+      }
+    }
+  }
+  
+  func index(_ request: Request) throws -> ResponseRepresentable {
+    let resources = try Resource.all()
+    return try resources.makeJSON()
+  }
+  
+  func show(_ request: Request) throws -> ResponseRepresentable {
+    let resource = try request.parameters.next(Resource.self)
+    return resource
+  }
+  
+  func create(_ request: Request) throws -> ResponseRepresentable {
+    guard let json = request.json else {
+      throw Abort.badRequest
+    }
+    let resource = try Resource(json: json)
+    try resource.save()
+    return resource
+  }
+  
+  func destroy(_ request: Request) throws -> ResponseRepresentable {
+    let resource = try request.parameters.next(Resource.self)
+    try resource.delete()
+    return JSON()
+  }
 }
