a7e40e56486759d63dc7e8ccce076aba18ef7fa0
Podoprosvetov Dmitry <ditansu@gmail.com>
2017-11-06 23:01:05 +0300

    add Jenkinsfile

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -9,7 +9,7 @@ extension Droplet {
         }
 
         get("plaintext") { req in
-            return "Hello, world!"
+            return "Hello, world2!"
         }
 
         // response to requests to /info domain
