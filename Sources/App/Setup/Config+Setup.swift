1ffb108b7dd3d13b78a5dbed79d31ac62f029a8a
Даниил Зорин <zorin.dania@yandex.ru>
2017-11-25 05:05:01 +0300

    Added all model types to preparation setup

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -2,24 +2,51 @@ import FluentProvider
 import MySQLProvider
 
 extension Config {
-    public func setup() throws {
-        // allow fuzzy conversions for these types
-        // (add your own types here)
-        Node.fuzzy = [Row.self, JSON.self, Node.self]
-
-        try setupProviders()
-        try setupPreparations()
-    }
+  public func setup() throws {
+    // allow fuzzy conversions for these types
+    // (add your own types here)
+    Node.fuzzy = [Row.self, JSON.self, Node.self]
     
-    /// Configure providers
-    private func setupProviders() throws {
-        try addProvider(FluentProvider.Provider.self)
-        try addProvider(MySQLProvider.Provider.self)
-    }
+    try setupProviders()
+    try setupPreparations()
+  }
+  
+  /// Configure providers
+  private func setupProviders() throws {
+    try addProvider(FluentProvider.Provider.self)
+    try addProvider(MySQLProvider.Provider.self)
+  }
+  
+  /// Add all models that should have their
+  /// schemas prepared before the app boots
+  private func setupPreparations() throws {
+    let entities: [Preparation.Type] = [
+      User.self,
+      Client.self,
+      Session.self,
+      Migration.self,
+      Event.self,
+      Place.self,
+      City.self,
+      EventReg.self,
+      EventRegField.self,
+      EventRegAnswer.self,
+      RegForm.self,
+      RegField.self,
+      RegFieldRule.self,
+      RegFieldAnswer.self,
+      Speech.self,
+      Speaker.self,
+      Content.self,
+      Creator.self,
+      GiveSpeech.self,
+      Social.self,
+      SocialAccount.self
+    ]
     
-    /// Add all models that should have their
-    /// schemas prepared before the app boots
-    private func setupPreparations() throws {
-        preparations.append(Post.self)
-    }
+    entities
+      .forEach {
+        preparations.append($0)
+      }
+  }
 }
