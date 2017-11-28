d486f0ca8d54bc0f3d9837e7193b8ec582b7aaad
Peter Tretyakov <petertretyakov@gmail.com>
2017-11-28 15:20:52 +0300

    Added RouteTests for authorized and unathorized requests - Added Responder methods with necessary headers (app-token, user-token) in extension - Added random string generator to String Extension - Re-indent AppTests Utilities.swift - Added authorization tests to RouteTests

---
+++ Tests/AppTests/Extensions/HTTP/Responder/Responder+Authorization.swift
@@ -0,0 +1,27 @@
+import HTTP
+
+extension Responder {
+  public func unauthorizedTestResponse(to method: HTTP.Method, at path: String, hostname: String = "0.0.0.0", headers: [HTTP.HeaderKey : String] = [:], body: BodyRepresentable? = nil, file: StaticString = #file, line: UInt = #line) throws -> HTTP.Response {
+    return try self.testResponse(
+      to: method,
+      at: path,
+      hostname: hostname,
+      headers: headers,
+      body: body,
+      file: file,
+      line: line
+    )
+  }
+  
+  public func appAuthorizedTestResponse(to method: HTTP.Method, at path: String, hostname: String = "0.0.0.0", headers: [HTTP.HeaderKey : String] = [:], body: BodyRepresentable? = nil, file: StaticString = #file, line: UInt = #line) throws -> HTTP.Response {
+    var appHeaders = headers
+    appHeaders["app-token"] = "test"
+    return try self.unauthorizedTestResponse(to: method, at: path, hostname: hostname, headers: appHeaders, body: body, file: file, line: line)
+  }
+  
+  public func userAuthorizedTestResponse(to method: HTTP.Method, at path: String, hostname: String = "0.0.0.0", headers: [HTTP.HeaderKey : String] = [:], body: BodyRepresentable? = nil, file: StaticString = #file, line: UInt = #line) throws -> HTTP.Response {
+    var userHeaders = headers
+    userHeaders["user-token"] = "user"
+    return try self.appAuthorizedTestResponse(to: method, at: path, hostname: hostname, headers: userHeaders, body: body, file: file, line: line)
+  }
+}
--- Tests/AppTests/RouteTests.swift
+++ Tests/AppTests/RouteTests.swift
@@ -5,35 +5,54 @@ import HTTP
 @testable import Vapor
 @testable import App
 
-/// This file shows an example of testing 
-/// routes through the Droplet.
-
 class RouteTests: TestCase {
-    let drop = try! Droplet.testable()
-    
-    func testHello() throws {
-        try drop
-            .testResponse(to: .get, at: "hello")
-            .assertStatus(is: .ok)
-            .assertJSON("hello", equals: "world")
-    }
-
-    func testInfo() throws {
-        try drop
-            .testResponse(to: .get, at: "info")
-            .assertStatus(is: .ok)
-            .assertBody(contains: "0.0.0.0")
-    }
+  let drop = try! Droplet.testable()
+  
+  func testUnathorizedRequest() throws {
+    try drop
+      .unauthorizedTestResponse(to: .get, at: "hello")
+      .assertStatus(is: .unauthorized)
+  }
+  
+  func testAppAuthorizedRequest() throws {
+    try drop
+      .appAuthorizedTestResponse(to: .get, at: "hello")
+      .assertStatus(is: .ok)
+  }
+  
+  func testFailedAppAuthorizedRequest() throws {
+    let randomToken = String.randomToken
+    try drop
+      .unauthorizedTestResponse(to: .get, at: "hello", headers: ["app-token": randomToken])
+      .assertStatus(is: .unauthorized)
+  }
+  
+  func testHello() throws {
+    try drop
+      .appAuthorizedTestResponse(to: .get, at: "hello")
+      .assertStatus(is: .ok)
+      .assertJSON("hello", equals: "world")
+  }
+  
+  func testInfo() throws {
+    try drop
+      .appAuthorizedTestResponse(to: .get, at: "info")
+      .assertStatus(is: .ok)
+      .assertBody(contains: "0.0.0.0")
+  }
 }
 
 // MARK: Manifest
 
 extension RouteTests {
-    /// This is a requirement for XCTest on Linux
-    /// to function properly.
-    /// See ./Tests/LinuxMain.swift for examples
-    static let allTests = [
-        ("testHello", testHello),
-        ("testInfo", testInfo),
-    ]
+  /// This is a requirement for XCTest on Linux
+  /// to function properly.
+  /// See ./Tests/LinuxMain.swift for examples
+  static let allTests = [
+    ("testHello", testHello),
+    ("testInfo", testInfo),
+    ("testUnathorizedRequest", testUnathorizedRequest),
+    ("testAppAuthorizedRequest", testAppAuthorizedRequest),
+    ("testFailedAppAuthorizedRequest", testFailedAppAuthorizedRequest)
+  ]
 }
