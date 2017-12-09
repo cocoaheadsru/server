0a72c5e795a58a97702736849c56fdf703db846c
Dmitry <ditansu@gmail.com>
2017-12-09 23:24:30 +0300

    Add route to HeartbeatController and config to Heartbeat's Model

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,23 +1,26 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
+  func setupRoutes() throws {
+    get("hello") { req in
+      var json = JSON()
+      try json.set("hello", "world")
+      return json
     }
+    
+    get("plaintext") { req in
+      return "Hello, world!"
+    }
+    
+    // response to requests to /info domain
+    // with a description of the request
+    get("info") { req in
+      return req.description
+    }
+    
+    get("description") { req in return req.description }
+    
+    let heartbeatController = HeartbeatController()
+    resource("heartbeat", heartbeatController)
+  }
 }
