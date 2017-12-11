f358b28713a8286cb1c3591a71f5a7f1fb6c30cc
Даниил Зорин <zorin.dania@yandex.ru>
2017-12-11 21:14:30 +0300

    Added static randomString property for String type

--- Tests/AppTests/Extensions/Foundation/String/String+RandomToken.swift
+++ Tests/AppTests/Extensions/Foundation/String/String+RandomToken.swift
@@ -1,13 +1,18 @@
 import Foundation
 
-extension String {  
-  static var invalidRandomToken: String {
+extension String {
+  
+  static var randomString: String {
     let uuid = UUID().uuidString
     let upperBound = UInt32(uuid.count - 1)
     let randomStringLength = Int(arc4random_uniform(upperBound)) + 1
     let randomStringIndex = String.Index(encodedOffset: randomStringLength)
-
-    let token = String(uuid[..<randomStringIndex])
+    let randString = String(uuid[..<randomStringIndex])
+    return randString
+  }
+    
+  static var invalidRandomToken: String {
+    let token = String.randomString
     if ["test", "user"].contains(token) {
       return self.invalidRandomToken
     }
