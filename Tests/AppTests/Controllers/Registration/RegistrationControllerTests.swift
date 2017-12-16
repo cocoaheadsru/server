eeab0a686c30e49b9b3eac0985eba3dbd8060b7a
Dmitry <ditansu@gmail.com>
2017-12-17 00:10:03 +0300

    added first test

--- Tests/AppTests/Controllers/Registration/RegistrationControllerTests.swift
+++ Tests/AppTests/Controllers/Registration/RegistrationControllerTests.swift
@@ -8,5 +8,18 @@ import Fluent
 
 class RegistrationControllerTests: TestCase {
   
+  let drop = try! Droplet.testable()
+  let controller = RegistrationController()
+  let validToken = TestConstants.Middleware.validToken
+  
+  func testThatRegFromGetNotFoundForWrongId() throws  {
+    let wrongId = -1
+    try drop
+      .userAuthorizedTestResponse(to: .get, at: "event/\(wrongId)/form")
+      .assertStatus(is: .notFound)
+  }
+  
+  func tes
+  
   
 }
