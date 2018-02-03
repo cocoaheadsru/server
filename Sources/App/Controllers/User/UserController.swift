32fa73454e35dc6268b6b6a9b0048ab9a8079292
Peter Tretyakov <petertretyakov@gmail.com>
2018-02-03 20:14:06 +0300

    [user] Added photo upload functionality to update method

--- Sources/App/Controllers/User/UserController.swift
+++ Sources/App/Controllers/User/UserController.swift
@@ -1,21 +1,62 @@
 import Vapor
 import FluentProvider
+import Foundation
 
 final class UserController {
+  
+  let droplet: Droplet!
+  
+  init(droplet: Droplet) {
+    self.droplet = droplet
+  }
 
   func show(_ request: Request, user: User) throws -> ResponseRepresentable {
+    try updateSessionToken(for: user)
+    return user
+  }
+
+  func update(_ request: Request, user: User) throws -> ResponseRepresentable {
+    try updateSessionToken(for: user)
+    try user.update(for: request)
+    if
+      let userId = user.id?.string,
+      let bytes = request.formData?["photo"]?.bytes,
+      let filename = request.formData?["photo"]?.filename {
+      try user.photo = newPhotoURL(for: userId, photoBytes: bytes, filename: filename)
+    }
+    try user.save()
+    return user
+  }
+  
+  func updateSessionToken(for user: User) throws {
     do {
       try user.updateSessionToken()
     } catch {
       throw Abort.badRequest
     }
-    return user
   }
-
-  func update(_ request: Request, user: User) throws -> ResponseRepresentable {
-    try user.update(for: request)
-    try user.save()
-    return user
+  
+  func newPhotoURL(for userId: String, photoBytes: [UInt8], filename: String) throws -> String {
+    let userDir = URL(fileURLWithPath: droplet.config.workDir)
+      .appendingPathComponent("images")
+      .appendingPathComponent("user_photos")
+      .appendingPathComponent(userId)
+    let fileManager = FileManager.default
+    
+    if fileManager.fileExists(atPath: userDir.path) {
+      let filePathes = try fileManager.contentsOfDirectory(atPath: userDir.path)
+      for path in filePathes {
+        let fullURL = userDir.appendingPathComponent(path)
+        try fileManager.removeItem(at: fullURL)
+      }
+    } else {
+      try fileManager.createDirectory(at: userDir, withIntermediateDirectories: true, attributes: nil)
+    }
+    let userDirWithImage = userDir.appendingPathComponent(filename)
+    let data = Data(bytes: photoBytes)
+    fileManager.createFile(atPath: userDirWithImage.path, contents: data, attributes: nil)
+    
+    return userDirWithImage.path
   }
 }
 
@@ -29,4 +70,8 @@ extension UserController: ResourceRepresentable {
   }
 }
 
-extension UserController: EmptyInitializable {}
+extension UserController: EmptyInitializable {
+  convenience init() throws {
+    try self.init()
+  }
+}
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -17,7 +17,9 @@ extension Droplet {
     try resource("event/:id/speech", EventSpeechController.self)
     try resource("event/:id/form", RegistrationFormController.self)
     try resource("event/register", RegistrationController.self)
-    try resource("user", UserController.self)
+    
+    let userController = UserController(droplet: self)
+    resource("user", userController)
     try resource("user", UserUnauthorizedController.self)
   }
 }
