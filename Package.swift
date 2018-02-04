ae1a5d640721af449aa426d2f7a07497e7acedb5
Dmitry <ditansu@gmail.com>
2018-02-04 16:04:25 +0300

    Added Auth-provider, minor updates to vapor, fluent, node, sockets

--- Package.swift
+++ Package.swift
@@ -12,10 +12,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/vapor/mysql-provider.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.1.1"))
+        .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.1.1")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "MySQLProvider"],
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "MySQLProvider", "AuthProvider"],
                 exclude: [
                     "Config",
                     "Public",
