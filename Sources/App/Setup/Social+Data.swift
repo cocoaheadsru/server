1446159ba08ea6faa2da025b4d4560d223f41408
Dmitry <ditansu@gmail.com>
2018-02-07 22:16:06 +0300

    Added social net's data recording  to the Social model during a drop configure

---
+++ Sources/App/Setup/Social+Data.swift
@@ -0,0 +1,21 @@
+import Fluent
+import Vapor
+
+extension Droplet {
+
+  func setupSocial() throws {
+    let socials = [
+      Social.Nets.fb,
+      Social.Nets.vk,
+      Social.Nets.git]
+
+    try socials.forEach { socialName in
+      if try Social.find(by: socialName) == nil {
+        let social = Social(name: socialName)
+        try social.save()
+      }
+    }
+
+  }
+
+}
