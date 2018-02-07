f9f5dfb0c5b843f4e59acf4946eef281958d1fc8
Dmitry <ditansu@gmail.com>
2018-02-07 22:04:38 +0300

    Refactored FacebookAuth (test and app) and UserAuthorizationController Added switch for socials networks

--- Sources/App/Controllers/UserAuthorization/UserAuthorizationController.swift
+++ Sources/App/Controllers/UserAuthorization/UserAuthorizationController.swift
@@ -4,95 +4,40 @@ import Fluent
 
 final class UserAuthorizationController {
 
-  let drop: Droplet
-  let config: Config
-
+  private let drop: Droplet
+  private let config: Config
+  private let fb: FacebookController
+  private let vk: VkontakteController
+  
   init(drop: Droplet) {
     self.drop = drop
     self.config = drop.config
+    fb = FacebookController(drop: self.drop)
+    vk = VkontakteController(drop: self.drop)
   }
 
   func store(_ req: Request) throws -> ResponseRepresentable {
 
-    func authorizateFacebookUser(by authorizateCode: String) throws -> String {
-
-      let authURL = config["facebook", "auth_url"]?.string ?? ""
-      let clientId = config["facebook", "client_id"]?.string ?? ""
-      let redirectURI = config["facebook", "redirect_uri"]?.string ?? ""
-      let scope = config["facebook", "scope"]?.string ?? ""
-      let clientSecret = config["facebook", "client_secret"]?.string ?? ""
-      let code = authorizateCode + "#_=_"
-
-      let authRequest = try drop.client.get(authURL, query: [
-        "client_id": clientId,
-        "redirect_uri": redirectURI,
-        "scope": scope,
-        "client_secret": clientSecret,
-        "code": code
-      ])
-
-      //UNCOMENT THIS AFTER DEBUG
-//      guard  let accessToken = authRequest.json?["access_token"]?.string else {
-//        throw Abort(.badRequest, reason: "Can't get 'access_token' from Facebook")
-//      }
-
-      // DELETE THIS AFTER DEBUG
-      let accessToken = config["facebook", "access_token"]?.string ?? ""
-
-      return accessToken
-
-    }
-
-    func createUserByFacebookProfile(use token: String) throws -> User {
-
-      let userInfoUrl =  config["facebook", "user_info_url"]?.string ?? ""
-      let fields  =  config["facebook", "fields"]?.string ?? ""
-      let scope = config["facebook", "scope"]?.string ?? ""
-
-      let userInfo = try drop.client.get(userInfoUrl, query: [
-        "fields": fields,
-        "scope": scope,
-        "access_token": token
-      ])
-
-      //DEBUG
-      let jsonUserInfo = userInfo.json!
-      print(try jsonUserInfo.serialize(prettyPrint: true).makeString())
-      //END DEBUG
-
-      guard
-        let socialUserId = userInfo.json?["id"]?.string,
-        let name = userInfo.json?["first_name"]?.string,
-        let lastname = userInfo.json?["first_name"]?.string,
-        let email = userInfo.json?["email"]?.string,
-        let photo = userInfo.json?["picture", "data", "url"]?.string
-      else {
-        throw Abort(.badRequest, reason: "Can't get user profile from Facebook")
-      }
-
-      let user = User(
-        name: name,
-        lastname: lastname,
-        company: "",
-        position: "",
-        photo: photo,
-        email: email,
-        phone: "")
-
-      try user.save()
-      return user
-    }
-
-    guard let code = req.json?["token"]?.string else {
+    guard let token = req.json?["token"]?.string else {
       throw Abort(.badRequest, reason: "Can't get 'token' from request")
     }
 
-    //FB
-    let accessToken = try authorizateFacebookUser(by: code)
-    let user = try  createUserByFacebookProfile(use: accessToken)
-    //
+    guard let social = req.json?["social"]?.string else {
+      throw Abort(.badRequest, reason: "Can't get 'social' from request")
+    }
 
-    return user
+    switch social {
+    case Social.Nets.fb:
+      return try fb.createOrUpdateUserProfileFromFacebook(use: token)
+    case Social.Nets.vk:
+      guard let secret = req.json?["secret"]?.string else {
+        throw Abort(.badRequest, reason: "Can't get 'secret' from request")
+      }
+      return try vk.createOrUpdateUserProfileFromVkontake(use: token, secret: secret)
+    default:
+      throw Abort(.badRequest, reason: "Wrong social id: \(social)")
+    }
+
   }
 
 }
--- Tests/AppTests/Common/Helpers/Controllers/Auth/FacebookAuthControllerTestHelper.swift
+++ Tests/AppTests/Common/Helpers/Controllers/Auth/FacebookAuthControllerTestHelper.swift
@@ -7,31 +7,23 @@ import Foundation
 final class FacebookAuthControllerTestHelper {
 
   static func getTestRequest(config: Config) throws -> JSON? {
-    guard let token = config["facebook", "code"]?.string else {
+    guard let token = config[Social.Nets.fb, "code"]?.string else {
       return nil
     }
-    let result = try JSON(node: ["token": token])
+    let result = try! JSON(
+      node: [
+        "token": token,
+        "social": Social.Nets.fb
+    ])
     return result
-  }
-
-  static func getUserSessionToken(by response: JSON) throws -> String? {
-    guard
-      let userId = response[User.Keys.id]?.int,
-      let user = try! User.find(userId),
-      let session = try! user.session()
-    else {
-      return nil
-    }
-
-    return session.token
   }
 
   static func getUserInfoFromSocial(drop: Droplet) throws -> JSON? {
     let config = drop.config
-    let userInfoUrl = config["facebook", "user_info_url"]?.string ?? ""
-    let fields = config["facebook", "fields"]?.string ?? ""
-    let scope = config["facebook", "scope"]?.string ?? ""
-    let token = config["facebook", "access_token"]?.string ?? ""
+    let userInfoUrl = config[Social.Nets.fb, "user_info_url"]?.string ?? ""
+    let fields = config[Social.Nets.fb, "fields"]?.string ?? ""
+    let scope = config[Social.Nets.fb, "scope"]?.string ?? ""
+    let token = config[Social.Nets.fb, "access_token"]?.string ?? ""
     
     let userInfo = try drop.client.get(userInfoUrl, query: [
       "fields": fields,
@@ -42,7 +34,7 @@ final class FacebookAuthControllerTestHelper {
     guard
       let socialUserId = userInfo.json?["id"]?.string,
       let name = userInfo.json?["first_name"]?.string,
-      let lastname = userInfo.json?["first_name"]?.string,
+      let lastname = userInfo.json?["last_name"]?.string,
       let email = userInfo.json?["email"]?.string,
       let photo = userInfo.json?["picture", "data", "url"]?.string
       else {
