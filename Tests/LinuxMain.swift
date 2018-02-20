618bab21e454549a2b087a7d9d43b2ac7d08e76f
Dmitry <ditansu@gmail.com>
2018-02-20 16:40:47 +0300

    Updated LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -132,14 +132,6 @@ extension UserPhotoTest {
    ("testThatUserPhotoIsUpdatedUsingPhotoAtURL", testThatUserPhotoIsUpdatedUsingPhotoAtURL)
   ]
 }
-extension VkontakteAuthControllerTest {
-  static var allTests: [(String, (VkontakteAuthControllerTest) -> () throws -> Void)] = [
-   ("testThatUserCreatedAndStoredFromVkontakteAccount", testThatUserCreatedAndStoredFromVkontakteAccount),
-   ("testThatSessionTokenCreatedAndStoredFromVkontakteAccount", testThatSessionTokenCreatedAndStoredFromVkontakteAccount),
-   ("testThatIfUserExistThenUserProfileUpdatedFromVkontakte", testThatIfUserExistThenUserProfileUpdatedFromVkontakte),
-   ("testThatUserPhotoFromVkontakteIsSaved", testThatUserPhotoFromVkontakteIsSaved)
-  ]
-}
 
 XCTMain([
   testCase(AutoapproveTest.allTests),
@@ -154,7 +146,6 @@ XCTMain([
   testCase(RouteTests.allTests),
   testCase(UserAuthByTokenTest.allTests),
   testCase(UserControllerTests.allTests),
-  testCase(UserPhotoTest.allTests),
-  testCase(VkontakteAuthControllerTest.allTests)
+  testCase(UserPhotoTest.allTests)
 ])
 #endif
